1ada219790cd8998db41abdfd2ee0ce51f46692f
osanbu82-svg <osanbu82@gmail.com>
2026-08-04 00:42:00 -0500

    Update socket.js

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,3 @@
-import { io } from 'socket.io-client';
-
-// Reemplaza esta URL con la que te de Render al desplegar tu backend
-export const socket = io('https://tu-backend-en-render.onrender.com', {
+export const socket = io('https://servitec-backend.onrender.com', {
   transports: ['websocket']
 });
